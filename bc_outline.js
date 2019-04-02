2882eed1027d549e4c0343f1e46f094a5f1efed8
Unknown <mrizo036@west-mec.org>
2019-04-01 18:21:25 -0700

    bc_outline.js completed
    All JavaScript within bc_outline.js completed. Event listener and setupStyles function created. Variables and setAttributes added within the function.

--- bc_outline.js
+++ bc_outline.js
@@ -26,8 +26,8 @@
 
 
 */
-//Generate an outline based on h1 through h6 heading in the source document
 
+//Generate an outline based on h1 through h6 heading in the source document
 window.addEventListener("load", makeOutline);
 
 function makeOutline() {
@@ -51,15 +51,51 @@ function makeOutline() {
 function createList(source, outlineList) {
       //Heading for the outline
       var headings = ["H1", "H2", "H3", "H4", "H5", "H6"];
+      //Previous level of the headings
+      var prevLevel = 0;
+      //Keep a running total of the article headings
+      var headNum = 0;
 
       //Loop through all of the child nodes of source article until no child nodes are left
       for (var n = source.firstChild; n !== null; n = n.nextSibling) {
             //Examine only article headings
             var headLevel = headings.indexOf(n.nodeName);
             if (headLevel !== -1) {
+                  //Add an id to the heading if it is missing
+                  headNum++;
+                  if (n.hasAttribute("id") === false) {
+                        n.setAttribute("id", "head" + headNum);
+                  }
                   var listElem = document.createElement("li");
-                  listElem.innerHTML = n.firstChild.nodeValue;
-                  outlineList.appendChild(listElem);
+                  //Create hypertext links to the document headings
+                  var linkElem = document.createElement("a");
+                  linkElem.innerHTML = n.innerHTML;
+                  linkElem.setAttribute("href", "#" + n.id);
+                  //Append the hypertext link to the list item
+                  listElem.appendChild(linkElem);
+                  if (headLevel === prevLevel) {
+                        //Append the list item to the current list
+                        outlineList.appendChild(listElem);
+                  } else if (headLevel > prevLevel) {
+                        //Start a new nested list
+                        var nestedList = document.createElement("ol");
+                        nestedList.appendChild(listElem);
+                        //Append nestedList to last item of the current list
+                        outlineList.lastChild.appendChild(nestedList);
+                        //Change the current list to the nested list
+                        outlineList = nestedList
+                  } else {
+                        //Append the list item to a higher list
+                        //Calculate the difference between the current and previous level
+                        var levelUp = prevLevel - headLevel;
+                        //go up to a higher level
+                        for (var i = 1; i <= levelUp; i++) {
+                              outlineList = outlineList.parentNode.parentNode;
+                        }
+                        outlineList.appendChild(listElem);
+                  }
+                  //Update the value of prevLevel
+                  prevLevel = headLevel;
             }
       }
 }
--- bc_switch.js
+++ bc_switch.js
@@ -16,3 +16,44 @@
    view and page view
    
 */
+
+window.addEventListener("load", setupStyles);
+
+function setupStyles() {
+    //Create a link element for the page view styles
+    var pageStyle = document.createElement("link");
+    pageStyle.setAttribute("href", "bc_page.css");
+    pageStyle.setAttribute("rel", "stylesheet");
+    pageStyle.setAttribute("disabled", "diabled");
+
+    //Append the link element to the document head
+    document.head.appendChild(pageStyle);
+    pageStyle.disabled = true;
+
+    //Insert buttons for the style switcher
+    var buttonDIV = document.createElement("div");
+    buttonDIV.setAttribute("id", "styleButtons");
+
+    var webButton = document.createElement("input");
+    webButton.setAttribute("type", "button");
+    webButton.setAttribute("value", "Web View");
+
+    var pageButton = document.createElement("input");
+    pageButton.setAttribute("type", "button");
+    pageButton.setAttribute("value", "Page View");
+
+    buttonDIV.appendChild(webButton);
+    buttonDIV.appendChild(pageButton);
+
+    document.body.insertBefore(buttonDIV, document.body.firstChild);
+
+    //Append an embedded style sheet to the document head
+    var buttonStyles = document.createElement("style");
+    document.head.appendChild(buttonStyles);
+
+    //Add style rules to the embedded style sheet
+    document.styleSheets[document.styleSheets.length - 1].insertRule(
+        "div#styleButtons { \
+            position: fixed; \
+        }", 0);
+}
